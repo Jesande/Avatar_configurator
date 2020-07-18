a97819923d11c80103b88d8a3dec0ece00703e45
Jesande <58031707+Jesande@users.noreply.github.com>
2020-07-18 12:39:23 +0200

    intial changes for simple configurator

--- main.js
+++ main.js
@@ -16,10 +16,21 @@ function face() {
   face1Img.style.display = 'none';
   face2Img.style.display = 'block';
   face3Img.style.display = 'none';
-  // reset( {
-  //   return false
-  //  })
+  // dont reset???
 };
+
+// when click on face1-btn = hide all the faces inside avatar-head-container & show face1
+// when click on face1-btn = check if face1Img is hide and delet the class ".hide" & add face2Img and face3Img the class "hide"
+
+// face2Btn.addEventListener("click", face);
+// function face() {
+//   face1Img.style.display = 'none';
+//   face2Img.style.display = 'block';
+//   face3Img.style.display = 'none';
+//   // reset( {
+//   //   return false
+//   //  })
+// };
 
 // face2Btn.addEventListener("click", faceSquare);
 // function faceSquare() {

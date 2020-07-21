3fc95d59d53273c8d4bd90717921202700912d83
Jesande <58031707+Jesande@users.noreply.github.com>
2020-07-21 07:56:11 +0200

    fix show face

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
 // var btn for shapes
-var face0 = document.getElementById('reset-img');
-var face1Btn = document.getElementById('btn-face1').addEventListener('click', showShape);
-var face2Btn = document.getElementById('btn-face2').addEventListener('click', showShape);
-var face3Btn = document.getElementById('btn-face3').addEventListener('click', showShape);
-var hair1Btn = document.getElementById('hair1-btn').addEventListener('click', showShape);
-var hair2Btn = document.getElementById('hair2-btn').addEventListener('click', showShape);
-var hair3Btn = document.getElementById('hair3-btn').addEventListener('click', showShape);
-var hair4Btn = document.getElementById('hair4-btn').addEventListener('click', showShape);
+document.getElementById('btn-face1').addEventListener('click', showFace);
+document.getElementById('btn-face2').addEventListener('click', showFace);
+document.getElementById('btn-face3').addEventListener('click', showFace);
+var hair1Btn = document.getElementById('hair1-btn').addEventListener('click', showHair);
+var hair2Btn = document.getElementById('hair2-btn').addEventListener('click', showHair);
+var hair3Btn = document.getElementById('hair3-btn').addEventListener('click', showHair);
+var hair4Btn = document.getElementById('hair4-btn').addEventListener('click', showHair);
 var eyes1Btn = document.getElementById('eyes1-btn').addEventListener('click', showShape);
 var eyes2Btn = document.getElementById('eyes2-btn').addEventListener('click', showShape);
 var eyes3Btn = document.getElementById('eyes3-btn').addEventListener('click', showShape);
 var eyes4Btn = document.getElementById('eyes4-btn').addEventListener('click', showShape);
 
-// SHOW SHAPES
-function showShape(event) {
-  // console.log(event);
-  let elementToShow = "#" + event.target.dataset.element + "-img";
+// SHOW FACES
+function showFace(event) {
+  console.log("#" + event.target.dataset.element + "-img");
 
-  gsap.to('.hide', { opacity: 0, duration: 0});
-  gsap.to(face0, { opacity: 0, duration:0 });
-  gsap.to(elementToShow, { opacity: 1, duration: .15 });
+  gsap.to(".reset-img", { opacity: 0, duration:0 });
+  gsap.to(".face", { opacity: 0, duration:0 });
+  
+  let faceToShow = "#" + event.target.dataset.element + "-img";
+  gsap.to( faceToShow, { opacity: 1, duration: .15 });
+}
+  
+// SHOW HAIR
+function showHair(event) {
+  console.log("#" + event.target.dataset.element + "-img");
+
+  let hairToShow = "#" + event.target.dataset.element + "-img";
+  gsap.to(".reset-img", { opacity: 0, duration:0 });
+  gsap.to(".face", { opacity: 0, duration:0 });
+  gsap.to( hairToShow, { opacity: 1, duration: .15 });
 }
   
 
@@ -34,6 +44,7 @@ var color8Btn = document.getElementById('color8-btn').addEventListener('click', 
 
 
 function colorTheHair(event) {
-  console.log(event.target.dataset.color)
-
+  let colorToUse = event.target.dataset.color;
+  let hairChoosen = //???? how to grab the hair that was choosen?
+  gsap.to(pickedHair, { fill: colorToUse, duration: .25 });
 }

0a258c0220336940acb2389a61c5631db80dc819
Jesande <58031707+Jesande@users.noreply.github.com>
2020-07-21 22:20:51 +0200

    add style to last btn clicked

--- main.js
+++ main.js
@@ -23,15 +23,27 @@ document.getElementById('eyes2-btn').addEventListener('click', showEyes);
 document.getElementById('eyes3-btn').addEventListener('click', showEyes);
 document.getElementById('eyes4-btn').addEventListener('click', showEyes);
 
+
+let lastFaceButtonClicked = document.getElementById('btn-face1');
+let lastHairButtonClicked = document.getElementById('hair1-btn');
+let lastEyesButtonClicked = document.getElementById('eyes1-btn');
+
+
 // SHOW FACES
 function showFace(event) {
   console.log("#" + event.target.dataset.element + "-img");
-
+  
   gsap.to(".reset-img", { opacity: 0, duration:0 });
   gsap.to(".face", { opacity: 0, duration:0 });
-
+  
   let faceToShow = "#" + event.target.dataset.element + "-img";
   gsap.to( faceToShow, { opacity: 1, duration: .15 });
+  
+  if (lastFaceButtonClicked) {
+    lastFaceButtonClicked.classList.remove('active');
+  }
+  event.target.classList.add('active');
+  lastFaceButtonClicked = event.target;
 }
   
 // SHOW HAIR
@@ -43,6 +55,12 @@ function showHair(event) {
   
   var hairToShow = "#" + event.target.dataset.element + "-img";
   gsap.to( hairToShow, { opacity: 1, duration: .15 });
+
+  if (lastHairButtonClicked) {
+    lastHairButtonClicked.classList.remove('active');
+  }
+  event.target.classList.add('active');
+  lastHairButtonClicked = event.target;
 }
 
 // SHOW EYES
@@ -54,6 +72,12 @@ function showEyes(event) {
   
   let eyesToShow = "#" + event.target.dataset.element + "-img";
   gsap.to( eyesToShow, { opacity: 1, duration: .15 });
+
+  if (lastEyesButtonClicked) {
+    lastEyesButtonClicked.classList.remove('active');
+  }
+  event.target.classList.add('active');
+  lastEyesButtonClicked = event.target;
 }
   
 
@@ -67,12 +91,20 @@ document.getElementById('color6-btn').addEventListener('click', colorTheHair);
 document.getElementById('color7-btn').addEventListener('click', colorTheHair);
 document.getElementById('color8-btn').addEventListener('click', colorTheHair);
 
+let lastColorButtonClicked = document.getElementById('color1-btn');
+
+
 // COLOR THE HAIR
 function colorTheHair(event) {
   console.log("#" + event.target.dataset.color);
 
   let colorToUse = event.target.dataset.color;
   gsap.to(".hair", { fill: colorToUse, duration: .25 });
+  if (lastColorButtonClicked) {
+    lastColorButtonClicked.classList.remove('color-active');
+  }
+  event.target.classList.add('color-active');
+  lastColorButtonClicked = event.target;
 }
 
 
